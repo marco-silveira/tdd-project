d7f293cfa542f662bd729794c09a8af6e9ae8eeb
marco alves <marco.alves@bancointer.com.br>
2022-05-22 14:03:35 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 1

--- lists/tests.py
+++ lists/tests.py
@@ -18,7 +18,7 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
