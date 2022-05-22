36f1e6f69ee519da5c785b7854b8686203acd3c3
marco alves <marco.alves@bancointer.com.br>
2022-05-22 14:06:53 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/views.py
+++ lists/views.py
@@ -10,3 +10,6 @@ def home_page(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
 
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
