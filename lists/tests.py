a28049ada5ae675558f3defe53e6c7481b825832
marco alves <marco.alves@bancointer.com.br>
2022-05-22 15:07:53 -0300

    Implementando o Novo Projeto: Cada Lista Deve Ter Uma URL

--- lists/tests.py
+++ lists/tests.py
@@ -41,18 +41,25 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        my_list = List.objects.create()
+        response = self.client.get(f'/lists/{my_list.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        my_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=my_list)
-        Item.objects.create(text='itemey 2', list=my_list)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
 
 class NewListTest(TestCase):
 
@@ -64,4 +71,6 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
