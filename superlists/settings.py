5981d36251ec46a0f97295ae03711fae395455a1
marco alves <marco.alves@bancointer.com.br>
2022-06-17 21:18:32 -0300

    Modelo de usuário mínimo

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Internationalization
